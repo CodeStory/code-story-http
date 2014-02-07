f3670a48a1abddab1baee8657e1aa025455939f3
ybonnel <ybonnel@gmail.com>
2014-02-07 11:01:01 +0100

    Fix #7 : all tests are OK under windows

--- src/test/java/net/codestory/http/testhelpers/WebServerRule.java
+++ src/test/java/net/codestory/http/testhelpers/WebServerRule.java
@@ -19,15 +19,56 @@ import net.codestory.http.*;
 
 import org.junit.rules.*;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
 public class WebServerRule extends ExternalResource {
   private final String previousProdMode = System.getProperty("PROD_MODE");
   private final WebServer server = new WebServer();
 
   @Override
-  protected void before() {
+  protected void before() throws IOException {
     System.setProperty("PROD_MODE", "true");
+    cleanCache();
+
 
     server.startOnRandomPort();
+  }
+
+  private void cleanCache() {
+    // Clean cache
+    try {
+      Files.walkFileTree(Paths.get(System.getProperty("user.home"), ".code-story", "cache"), new SimpleFileVisitor<Path>() {
+
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+          Files.delete(file);
+          return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+          if (exc == null) {
+            Files.delete(dir);
+            return FileVisitResult.CONTINUE;
+          }
+          else {
+            // directory iteration failed; propagate exception
+            throw exc;
+          }
+        }
+      });
+    }
+    catch (IOException ignore) {
+
+    }
   }
 
   @Override
